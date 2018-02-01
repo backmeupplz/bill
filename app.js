3e3b47e595688d3f864d444b9f8e5ae8b4bfa224
Nikita Kolmogorov <backmeupplz@gmail.com>
2018-02-01 14:52:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ async function getSheet() {
   return new Promise((res, rej) => {
     sheets.spreadsheets.values.get({
       spreadsheetId,
-      range: ['A2:N23'],
+      range: ['A2:N24'],
       auth: jwtClient
     }, (err, response) => {
       if (err) { 
